faaaa4b2c31119ec4206729754d3b348cc995bfc
darriub15 <darriusabrown@gmail.com>
2023-09-26 13:28:13 -0700

    down, left, and right functions working

--- tetrisrevise.js
+++ tetrisrevise.js
@@ -19,53 +19,66 @@ board = [
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0],
     [0,0,0,0,0,0,0,0,0,0]
-    ]
+    ];
 
 l_piece = [ [0,2,0,0],
             [0,2,0,0],
             [0,2,2,0],
-            [0,0,0,0]]
-halfPoint = Math.floor(board[0].length / 2) - 1;
+            [0,0,0,0]];
+
+const blank_piece = [   [0,0,0,0],
+                        [0,0,0,0],
+                        [0,0,0,0],
+                        [0,0,0,0]];
 let pieceX = 0;
 let pieceY = 0;
-coors = [halfPoint, 0];
-startX = coors[0];
-endX = startX + 4;
-startY = coors[1];
-endY = startY + 4;
+let coors = [4, 0];
+let startX = coors[0];
+let endX = startX + 4;
+let startY = coors[1];
+let endY = startY + 4;
 const printBoard = () => {
     for (const index in board) {
-        console.log(board[index] + index)
+        console.log(board[index] + index);
     }
 }
 
-const draw = (piece) => {
+const draw = (piece, bool) => {
     for(let x = coors[0]; x < coors[0] + 4; x++) {
-        pieceY = 0
+        pieceY = 0;
         for (let y = coors[1]; y < coors[1] + 4; y++) {
             board[y][x] = piece[pieceY][pieceX]
-            pieceY += 1
+            pieceY += 1;
         }
-        pieceX += 1
+        pieceX += 1;
     }
     pieceX = 0;
     pieceY = 0;
-
-   printBoard();
+    if (bool === true) {
+        printBoard();
+    }
 }
 
 const down = () => {
-    //Settings the area above the piece to blank
-    for (let x = coors[0]; x < coors[0] + 4; x++) {
-        board[coors[1]][x] = 0
-        pieceY += x
-    }
-    
-    //Adjust coordinates, then redraw
-    coors[1] += 1
-    draw(l_piece)
+    draw(blank_piece, false);
+    coors[1] += 1;
+    draw(l_piece, true);
 }
 
-draw(l_piece)
-down()
-down()
+const right = () => {  
+    draw(blank_piece, false);
+    coors[0] += 1;
+    draw(l_piece, true);
+}
+
+const left = () => {
+    draw(blank_piece, false)
+    coors[0] -= 1;
+    draw(l_piece, true)
+}
+
+draw(l_piece, true);
+down();
+down();
+right()
+left()
